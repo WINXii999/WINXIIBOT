c50266a65008f4d8a0b61c2a51f2b957413336a1
WINXii999 <155809676+WINXii999@users.noreply.github.com>
2024-01-06 13:44:31 -0600

    menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -19,60 +19,58 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
-║    ◉— *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* —◉
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
-║➤ *𝗛ola, ${taguser}*
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
-╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+    const str = `
+💗WINXII BOT💗
 
-┏━━━━━━━━━━━━━━━━┓
-┃ *<𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒/>*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣*- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
-┣ ඬ⃟🔊 _Quien es tu sempai botsito 7w7_
-┣ ඬ⃟🔊 _Te diagnostico con gay_
-┣ ඬ⃟🔊 _No digas eso papu_
-┣ ඬ⃟🔊 _A nadie le importa_
-┣ ඬ⃟🔊 _Fiesta del admin_
-┣ ඬ⃟🔊 _Fiesta del administrador_ 
-┣ ඬ⃟🔊 _Vivan los novios_
-┣ ඬ⃟🔊 _Feliz cumpleaños_
-┣ ඬ⃟🔊 _Noche de paz_
-┣ ඬ⃟🔊 _Buenos dias_
-┣ ඬ⃟🔊 _Buenos tardes_
-┣ ඬ⃟🔊 _Buenos noches_
-┣ ඬ⃟🔊 _Audio hentai_
-┣ ඬ⃟🔊 _Chica lgante_
-┣ ඬ⃟🔊 _Feliz navidad_
-┣ ඬ⃟🔊 _Vete a la vrg_
-┣ ඬ⃟🔊 _Pasa pack Bot_
-┣ ඬ⃟🔊 _Atencion grupo_
-┣ ඬ⃟🔊 _Marica quien_
-┣ ඬ⃟🔊 _Murio el grupo_
-┣ ඬ⃟🔊 _Oh me vengo_
-┣ ඬ⃟🔊 _tio que rico_
-┣ ඬ⃟🔊 _Viernes_
-┣ ඬ⃟🔊 _Baneado_
-┣ ඬ⃟🔊 _Sexo_
-┣ ඬ⃟🔊 _Hola_
-┣ ඬ⃟🔊 _Un pato_
-┣ ඬ⃟🔊 _Nyanpasu_
-┣ ඬ⃟🔊 _Te amo_
-┣ ඬ⃟🔊 _Yamete_
-┣ ඬ⃟🔊 _Bañate_
-┣ ඬ⃟🔊 _Es puto_
-┣ ඬ⃟🔊 _La biblia_
-┣ ඬ⃟🔊 _Onichan_
-┣ ඬ⃟🔊 _Mierda de Bot_
-┣ ඬ⃟🔊 _Siuuu_
-┣ ඬ⃟🔊 _Epico_
-┣ ඬ⃟🔊 _Shitpost_
-┣ ඬ⃟🔊 _Rawr_
-┣ ඬ⃟🔊 _UwU_
-┣ ඬ⃟🔊 _:c_
-┣ ඬ⃟🔊 _a_
-┗━━━━━━━━━━━━━━━━┛`.trim();
+*𝗛ola, ${taguser}*
+
+
+*𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒*
+
+*- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
+🔊 _Quien es tu sempai botsito 7w7_
+🔊 _Te diagnostico con gay_
+🔊 _No digas eso papu_
+🔊 _A nadie le importa_
+🔊 _Fiesta del admin_
+🔊 _Fiesta del administrador_ 
+🔊 _Vivan los novios_
+🔊 _Feliz cumpleaños_
+🔊 _Noche de paz_
+🔊 _Buenos dias_
+🔊 _Buenos tardes_
+🔊 _Buenos noches_
+🔊 _Audio hentai_
+🔊 _Chica lgante_
+🔊 _Feliz navidad_
+🔊 _Vete a la vrg_
+🔊 _Pasa pack Bot_
+🔊 _Atencion grupo_
+🔊 _Marica quien_
+🔊 _Murio el grupo_
+🔊 _Oh me vengo_
+🔊 _tio que rico_
+🔊 _Viernes_
+🔊 _Baneado_
+🔊 _Sexo_
+🔊 _Hola_
+🔊 _Un pato_
+🔊 _Nyanpasu_
+🔊 _Te amo_
+🔊 _Yamete_
+🔊 _Bañate_
+🔊 _Es puto_
+🔊 _La biblia_
+🔊 _Onichan_
+🔊 _Mierda de Bot_
+🔊 _Siuuu_
+🔊 _Epico_
+🔊 _Shitpost_
+🔊 _Rawr_
+🔊 _UwU_
+🔊 _:c_
+🔊 _a_
+`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
